d6a8c0c651e8367eafc89c942091d6269399ee2a
yoshifumi <yoshifumi@kobain.local>
2021-08-12 15:17:53 +0900

    add todo3 to Strings

--- src/main/java/ch7/sec5/Strings.java
+++ src/main/java/ch7/sec5/Strings.java
@@ -1,14 +1,14 @@
 package ch7.sec5;
 
-// TODO3: add more signature after you read effective java item 17 
+// TODO 3: add more signature after you read effective java item 17 
 public class Strings {
 
-	// TODO2: refactor this constructor after you read effective java item 4
+	// TODO 2: refactor this constructor after you read effective java item 4
 	public Strings() {
 
 	} 
 
-	// TODO1: refactor this method after you read Readable Code 7.5,7.7 and 9.1
+	// TODO 1: refactor this method after you read Readable Code 7.5,7.7 and 9.1
 	public static String capitalize(String str) {
 		String ret = str;
 		if (str == null) {
