0aaf0d2a5261b670901855d330fd2d038bb3592c
yoshifumi <yoshifumi@kobain.local>
2021-08-13 16:38:52 +0900

    modify StringAnswer

--- src/main/java/ch7/sec5/StringsAnswer.java
+++ src/main/java/ch7/sec5/StringsAnswer.java
@@ -14,9 +14,9 @@ public final class StringsAnswer {
 
 		if (str.length() == 1) {
 			return str.toUpperCase();
-		} else {
-			return str.substring(0, 1).toUpperCase() + str.substring(1);
 		}
+
+		return str.substring(0, 1).toUpperCase() + str.substring(1);
 
 	}
 
