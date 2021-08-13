c7e768fc9a0da027dd68e0b298e57e6251b9ec07
yoshifumi <yoshifumi@kobain.local>
2021-08-13 16:35:38 +0900

    add StringAnswer

---
+++ src/main/java/ch7/sec5/StringsAnswer.java
@@ -0,0 +1,23 @@
+package ch7.sec5;
+
+public final class StringsAnswer {
+
+	private StringsAnswer() {
+
+	}
+
+	public static String capitalize(String str) {
+
+		if (str == null || str.length() == 0) {
+			return str;
+		}
+
+		if (str.length() == 1) {
+			return str.toUpperCase();
+		} else {
+			return str.substring(0, 1).toUpperCase() + str.substring(1);
+		}
+
+	}
+
+}
