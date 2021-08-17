0e823c82b08a6af2b3ff4dd949d0db27d3084492
kobain-jp <55679081+kobain-jp@users.noreply.github.com>
2021-08-17 15:56:35 +0900

    Update MarkDownUtils.java

--- src/main/java/ch4/sec7/MarkDownUtils.java
+++ src/main/java/ch4/sec7/MarkDownUtils.java
@@ -3,15 +3,13 @@ package ch4.sec7;
 import java.util.List;
 
 public class MarkDownUtils {
-	// 1st
 	// TODO1 : use early return:Readable code 7.5, 7.7 and remove the 1 nest(you might need to change if condition)
 	// * you do not need to improve exception handling
 	// TODO2 : add line break after you read Readable code 4.7
 	// TODO3 : re-name the variables after you read Readable code 2.7
 	// TODO4 : re-name method as it is after you read Readable code 3.0, 3.9
 	// TODO5 : move the variables after you read Readable code 9.2(定義の位置を下げる）
-	// 2nd
-	// TODO6 : use com.google.common.base.Strings to replace your line after you read Readable code 12.2 and Google DRY and OAOO
+	// TODO6 : use com.google.common.base.Strings to replace your code after you read Readable code 12.2 and search DRY and OAOO on internet
 	// TODO7 : find repeated coding which does not follow DRY and OAOO and extract method and follow them.
 	// TODO8 : extract method after you read Readable code 11.1
 	public static String createTables(List<String> l, int c) throws IllegalArgumentException {
