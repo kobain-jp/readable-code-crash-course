709859148c355a7bd3c5b7b0030d971aee1f53e8
kobain-jp <55679081+kobain-jp@users.noreply.github.com>
2021-08-24 23:08:58 +0900

    remove unnecessary import

--- src/test/java/readablecode/week3/MarkDownTableUtilsTest.java
+++ src/test/java/readablecode/week3/MarkDownTableUtilsTest.java
@@ -2,7 +2,6 @@ package readablecode.week3;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
