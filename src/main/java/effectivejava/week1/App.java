fac533c4377ecc531fc2eded2dc87270e36ef45f
yoshifumi <yoshifumi@kobain.local>
2021-08-25 00:13:54 +0900

    add TODO9 for effectivejava week1

--- src/main/java/effectivejava/week1/App.java
+++ src/main/java/effectivejava/week1/App.java
@@ -9,14 +9,15 @@ public class App {
 
 	public static void main(String[] args) {
 
-		//TODO7 : Effective Java Item 64
+		// TODO7 : Effective Java Item 64
 		MockEmployeeDao dao = new MockEmployeeDao();
 		@SuppressWarnings("rawtypes")
 		List employees = dao.findAll();
 
-		for (Object object : employees) {
+		// TODO9 : Effective Java Item 58 
+		for (int i = 0; i < employees.size(); i++) {
 			// TODO8 refactor here after you fix todo8 on Employee
-			Employee employee = (Employee) object;
+			Employee employee = (Employee) employees.get(i);
 			System.out.println(employee.printLine());
 		}
 
