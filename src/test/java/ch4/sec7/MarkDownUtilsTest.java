7c1b89eb8ed0931f8df9304bc8a3e82633bd1d35
yoshifumi <yoshifumi@kobain.local>
2021-08-24 21:04:22 +0900

    add test method for exception on MarkDownUtilsTest.java

--- src/test/java/ch4/sec7/MarkDownUtilsTest.java
+++ src/test/java/ch4/sec7/MarkDownUtilsTest.java
@@ -2,10 +2,11 @@ package ch4.sec7;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,18 +15,53 @@ import com.google.common.collect.ImmutableList;
 class MarkDownUtilsTest {
 
 	@Test
+	@DisplayName("第一引数がnull")
+	void testCreateTable_ColumnCaptionIsNull() {
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			MarkDownUtils.createTables(null, 1);
+		});
+
+	}
+
+	@Test
+	@DisplayName("第一引数が空のリスト")
+	void testCreateTable_ColumnCaptionIsEmpty() {
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			MarkDownUtils.createTables(new ArrayList<String>(), 1);
+		});
+
+	}
+
+	@Test
+	@DisplayName("第二引数が0以下")
+	void testCreateTable_EmptyRowCountIsZero() {
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			MarkDownUtils.createTables(new ArrayList<String>(), 0);
+		});
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			MarkDownUtils.createTables(new ArrayList<String>(), -1);
+		});
+
+	}
+
+	@Test
 	@DisplayName("単数のカラムと値が空の行を1行を持つテーブル")
 	void testCreateTable_SingleColumnWithSingleEmptyRow() throws IllegalArgumentException {
 
 		List<String> columnCaptions = ImmutableList.of("COL1");
 		int emptyRowCount = 1;
-		String actualText = MarkDownUtils.createTables(columnCaptions, emptyRowCount);
+		String actualMarkDownText = MarkDownUtils.createTables(columnCaptions, emptyRowCount);
 
 		String expectedHeader = "|COL1|" + System.lineSeparator();
 		String expectedSeparator = "|----|" + System.lineSeparator();
 		String expectedRecoreds = "|    |" + System.lineSeparator();
+		String expectedMarkDownText = expectedHeader + expectedSeparator + expectedRecoreds;
 
-		assertThat(actualText, is(expectedHeader + expectedSeparator + expectedRecoreds));
+		assertThat(actualMarkDownText, is(expectedMarkDownText));
 
 	}
 
@@ -35,13 +71,14 @@ class MarkDownUtilsTest {
 
 		List<String> columnCaptions = ImmutableList.of("COL1", "COL2");
 		int emptyRowCount = 2;
-		String actualText = MarkDownUtils.createTables(columnCaptions, emptyRowCount);
+		String actualMarkDownText = MarkDownUtils.createTables(columnCaptions, emptyRowCount);
 
 		String expectedHeader = "|COL1|COL2|" + System.lineSeparator();
 		String expectedSeparator = "|----|----|" + System.lineSeparator();
 		String expectedRecoreds = "|    |    |" + System.lineSeparator() + "|    |    |" + System.lineSeparator();
 
-		assertThat(actualText, is(expectedHeader + expectedSeparator + expectedRecoreds));
+		assertThat(actualMarkDownText, is(expectedHeader + expectedSeparator + expectedRecoreds));
 
 	}
+
 }
