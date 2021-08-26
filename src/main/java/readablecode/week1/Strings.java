e452211c3bc2e0b927d246440c0b9a59add3bca2
kobain-jp <55679081+kobain-jp@users.noreply.github.com>
2021-08-26 22:30:05 +0900

    Update Strings.java

--- src/main/java/readablecode/week1/Strings.java
+++ src/main/java/readablecode/week1/Strings.java
@@ -1,12 +1,6 @@
 package readablecode.week1;
 
-// TODO 3: add more signature after you read effective java item 17 
 public class Strings {
-
-	// TODO 2: refactor this constructor after you read effective java item 4
-	public Strings() {
-
-	} 
 
 	// TODO 1: refactor this method after you read Readable Code 7.5,7.7 and 9.1
 	public static String capitalize(String str) {
