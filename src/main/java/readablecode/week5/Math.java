90ec59353ac6737d22c29041bc0beadc9befdc63
yoshifumi <yoshifumi@kobain.local>
2021-09-01 17:30:49 +0900

    fix

--- src/main/java/readablecode/week5/Math.java
+++ src/main/java/readablecode/week5/Math.java
@@ -2,8 +2,8 @@ package readablecode.week5;
 
 public class Math {
 
-	// TODO1 7.4 
-	public boolean isNagativeValue(int value) {
+	// TODO1 7.4
+	public boolean isNagativeValue1(int value) {
 
 		if (0 > value) {
 			return true;
@@ -13,9 +13,9 @@ public class Math {
 
 	}
 
-	public boolean isPositiveValue(int value) {
+	public boolean isNagativeValue2(int value) {
 
-		if (value > 0) {
+		if (value < 0) {
 			return true;
 		} else {
 			return false;
@@ -26,6 +26,22 @@ public class Math {
 	public boolean isZero(int value) {
 		// TODO2 7.3
 		return 0 == value ? true : false;
+	}
+
+	public boolean isZero2(int value) {
+
+		if (0 == value) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public boolean isZero3(int value) {
+
+		return 0 == value;
+
 	}
 
 }
