25461dfa98d8d2e23b0f8b5b3523a4cbd71b4be5
kobain-jp <55679081+kobain-jp@users.noreply.github.com>
2021-09-02 00:13:01 +0900

    Update StringsAnswer2.java

--- src/main/java/readablecode/week1/StringsAnswer2.java
+++ src/main/java/readablecode/week1/StringsAnswer2.java
@@ -9,6 +9,7 @@ public class StringsAnswer2 {
 		}
 
 		// it work but not readable because reader might not know 'str.substring(1)' return ""
+		// and might confirm substring logic
 		return str.substring(0, 1).toUpperCase() + str.substring(1);
 
 	}
