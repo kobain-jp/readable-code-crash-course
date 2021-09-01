4074ef76f01e5637e8e7003fb256b5eff0905a17
kobain-jp <55679081+kobain-jp@users.noreply.github.com>
2021-09-02 00:11:06 +0900

    Update MarkdownUtils.java

--- src/main/java/readablecode/week2/MarkdownUtils.java
+++ src/main/java/readablecode/week2/MarkdownUtils.java
@@ -7,11 +7,16 @@ public class MarkdownUtils {
 	// might need to change if condition and if condition is complicated such as
 	// if(!(...)) please refactor it after you read 7.2)
 	// * you do not need to improve exception handling
+	
 	// TODO2 : add line break and comment for each block/paragraph after you read
 	// Readable code 4.7
+	
 	// TODO3 : re-name the variables after you read Readable code 2.2 2.7, 3.8
+	
 	// TODO4 : re-name method as it is after you read Readable code 3.0, 3.9
+	
 	// TODO5 : remove the unnecessary variable after you read Readable code 9.1
+	
 	public static String createTables(List<String> l, int c) throws IllegalArgumentException {
 		if (l != null && !l.isEmpty() && c > 0) {
 			StringBuilder tmp = new StringBuilder();
