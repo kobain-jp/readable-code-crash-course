e3d6182b31664e008268222f4d139340b7310b7e
kakichi_y <kakichi_y@works-hi.co.jp>
2022-12-21 09:49:13 +0900

    week1 first commit

--- src/main/java/readablecode/week1/Strings.java
+++ src/main/java/readablecode/week1/Strings.java
@@ -2,21 +2,11 @@ package readablecode.week1;
 
 public class Strings {
 
-	// TODO 1: 7.5,7.7 and 9.1
 	public static String capitalize(String str) {
-		String ret = str;
-		if (str == null) {
-			ret = str;
-		} else if (str.length() == 0) {
-			ret = str;
-		} else {
-			if (str.length() == 1) {
-				ret = str.toUpperCase();
-			} else {
-				ret = str.substring(0, 1).toUpperCase() + str.substring(1);
-			}
+		if (str == null || str == "") {
+			return str;
 		}
-		return ret;
-	}
 
+		return str.substring(0, 1).toUpperCase() + str.substring(1);
+	}
 }
